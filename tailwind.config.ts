d2a03d7679d3e196bacb6ff64a8c7eb862e508a0
Chuks <63956312+skido600@users.noreply.github.com>
2025-01-28 01:34:10 +0100

    try

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,4 @@
+/* eslint-disable */
 const defaultTheme = require("tailwindcss/defaultTheme");
 const svgToDataUri = require("mini-svg-data-uri");
 const colors = require("tailwindcss/colors");
@@ -25,20 +26,26 @@ module.exports = {
   },
   plugins: [
     addVariablesForColors,
-    function ({ matchUtilities, theme }) {
+    function ({
+      matchUtilities,
+      theme,
+    }: {
+      matchUtilities: any;
+      theme: (path: string) => any;
+    }) {
       matchUtilities(
         {
-          "bg-grid": (value) => ({
+          "bg-grid": (value: string) => ({
             backgroundImage: `url("${svgToDataUri(
               `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
             )}")`,
           }),
-          "bg-grid-small": (value) => ({
+          "bg-grid-small": (value: string) => ({
             backgroundImage: `url("${svgToDataUri(
               `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="8" height="8" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
             )}")`,
           }),
-          "bg-dot": (value) => ({
+          "bg-dot": (value: string) => ({
             backgroundImage: `url("${svgToDataUri(
               `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="1.6257413380501518"></circle></svg>`
             )}")`,
@@ -50,7 +57,13 @@ module.exports = {
   ],
 };
 
-function addVariablesForColors({ addBase, theme }) {
+function addVariablesForColors({
+  addBase,
+  theme,
+}: {
+  addBase: any;
+  theme: (path: string) => any;
+}) {
   const allColors = theme("colors");
   const newVars = Object.fromEntries(
     Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
